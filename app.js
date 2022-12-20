2d32080550feb48ce783064fe35e115a295eeeac
Alejandro Aliaga <alejandro.aliaga@vipre.com>
2022-12-20 18:21:29 -0300

    Some code refactoring

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const { Order } = require('./models/order')
- 
+const orderHandler = require('./handlers/order-handler')
+
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
@@ -19,88 +19,13 @@ app.use(cors({
 let testOrders = []
 
 // COFFEE ORDERS
-app.post('/test/new-order', (req, res) => {
-    console.debug(`New Order`)
-    
-    const name = req.body.name
-    const coffeeName = req.body.coffeeName
-    const total = parseFloat(req.body.total)
-    const size = req.body.size
+app.post('/test/new-order', orderHandler.newOrder)
+app.put('/test/orders/:id', orderHandler.updateOrder)
+app.delete('text/orders/:id', orderHandler.deleteOrder)
+app.get('/test/orders', orderHandler.getAllOrders)
+app.get('/test/clear-orders', orderHandler.clearOrders)
 
-    if (name && coffeeName && total && size) {
-        let order = new Order(name, coffeeName, total, size)
-        testOrders.push(order)
-
-        order.id = testOrders.length + 1
-
-        res.status(200)
-        .json(order)
-
-    } else {
-        res.status(400)
-        .json({
-            success: false,
-            message: 'Missing fields'
-        })
-    }
-})
-
-app.put('/test/orders/:id', (req, res) => {
-    const orderId = parseInt(req.params.id)
-    console.debug(req.body)
-
-    const name = req.body.name
-    const coffeeName = req.body.coffeeName
-    const total = parseFloat(req.body.total)
-    const size = req.body.size
-
-    // get the order you want to update
-    let updateOrder = testOrders.find(order => order.id == orderId)
-
-    if (updateOrder) {
-        updateOrder.name = name
-        updateOrder.coffeeName = coffeeName
-        updateOrder.total = total
-        updateOrder.size = size
-
-        console.debug(`Order Detail: \n${updateOrder}`)
-
-        res.status(200)
-            .json(updateOrder)
-
-    } else {
-        res.status(400)
-            .json({
-                success: false,
-                message: 'Wrong order!'
-            })
-    }
-})
-
-app.delete('text/orders/:id', (req, res) => {
-    const orderId = parseInt(req.params.id)
-    console.debug(`Order to delete: ${orderId}`)
-
-    const order = testOrders.find(order => order.id == orderId)
-    testOrders = testOrders.filter(order => order.id != order.id)
-
-    res.status(200)
-        .json(order)
-})
-
-app.get('/test/orders', (_, res) => {
-    res.status(200)
-        .json(testOrders)
-})
-
-app.get('/test/clear-orders', (_, res) => {
-    testOrders = []
-    res.status(200)
-        .json({
-            message: 'Orders have been cleared!'
-        })
-})
-
+// Port Configuration
 const port = process.env.PORT || 3000
 // Start listening
 app.listen(port, () => {
---
+++ handlers/order-handler.js
@@ -0,0 +1,126 @@
+const { Order } = require('../models/order')
+
+let orders = [
+    {
+        id: 1,
+        name: 'John Doe',
+        coffeeName: 'Hot Coffee',
+        total: 4.5,
+        size: 'Medium'
+    }, 
+    {
+        id: 2,
+        name: 'Mary Doe',
+        coffeeName: 'Frappuccino',
+        total: 5.6,
+        size: 'Small'
+    }
+]
+
+let testOrders = [
+    {
+        id: 1,
+        name: 'John Doe',
+        coffeeName: 'Hot Coffee',
+        total: 4.5,
+        size: 'Medium'
+    }, 
+    {
+        id: 2,
+        name: 'Mary Doe',
+        coffeeName: 'Frappuccino',
+        total: 5.6,
+        size: 'Small'
+    }
+]
+
+// COFFEE ORDERS
+const newOrder = async (req, res) => {
+    console.debug(`New Order`)
+    
+    const name = req.body.name
+    const coffeeName = req.body.coffeeName
+    const total = parseFloat(req.body.total)
+    const size = req.body.size
+
+    if (name && coffeeName && total && size) {
+        let order = new Order(name, coffeeName, total, size)
+        testOrders.push(order)
+
+        order.id = testOrders.length + 1
+
+        res.status(200)
+        .json(order)
+
+    } else {
+        res.status(400)
+        .json({
+            success: false,
+            message: 'Missing fields'
+        })
+    }
+}
+
+const updateOrder = async (req, res) => {
+    const orderId = parseInt(req.params.id)
+    console.debug(req.body)
+
+    const name = req.body.name
+    const coffeeName = req.body.coffeeName
+    const total = parseFloat(req.body.total)
+    const size = req.body.size
+
+    // get the order you want to update
+    let updateOrder = testOrders.find(order => order.id == orderId)
+
+    if (updateOrder) {
+        updateOrder.name = name
+        updateOrder.coffeeName = coffeeName
+        updateOrder.total = total
+        updateOrder.size = size
+
+        console.debug(`Order Detail: \n${updateOrder}`)
+
+        res.status(200)
+            .json(updateOrder)
+
+    } else {
+        res.status(400)
+            .json({
+                success: false,
+                message: 'Wrong order!'
+            })
+    }
+}
+
+const deleteOrder = async (req, res) => {
+    const orderId = parseInt(req.params.id)
+    console.debug(`Order to delete: ${orderId}`)
+
+    const order = testOrders.find(order => order.id == orderId)
+    testOrders = testOrders.filter(order => order.id != order.id)
+
+    res.status(200)
+        .json(order)
+}
+
+const getAllOrders = async (_, res) => {
+    res.status(200)
+        .json(testOrders)
+}
+
+const clearOrders = async (_, res) => {
+    testOrders = []
+    res.status(200)
+        .json({
+            message: 'Orders have been cleared!'
+        })
+}
+
+module.exports = { 
+    newOrder, 
+    deleteOrder, 
+    updateOrder, 
+    getAllOrders, 
+    clearOrders
+}
